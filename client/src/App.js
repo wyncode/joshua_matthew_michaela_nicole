db76e875bb0f4cda0b7a2a918fee43756b3ffb81
Michaela Carrera <carrera.michaela104@gmail.com>
2019-07-13 16:11:28 -0400

    Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 // import Drink from './pages/Drink'
 
 const App = () => {
  return (
    <BrowserRouter>
+      <Navbar/>
      <Route exact path="/" component={Home} />
      {/* <Route exact path="/" component={Drink} /> */}
    </BrowserRouter>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const Navbar = () => (
+   <nav>
+        <ul>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink exact to ="/random-drink">Random Drink</NavLink></li>
+        </ul>
+
+    </nav>
+  )
+
+
+
+
+
+
+
+export default Navbar
