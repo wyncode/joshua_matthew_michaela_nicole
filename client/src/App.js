f3ad58b2a99ce55857a4c7e47f5f705bec1a39cf
Nicole Cuellar <cuellar.m.nicole@gmail.com>
2019-07-13 18:46:55 -0400

    drink page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import MyDrink from './pages/Drink'
 // import Drink from './pages/Drink'
 
 const App = () => {
@@ -11,9 +11,11 @@ const App = () => {
    <BrowserRouter>
       <Navbar/>
      <Route exact path="/" component={Home} />
+     <Route exact path="/MyDrink" component={MyDrink} />
      {/* <Route exact path="/" component={Drink} /> */}
    </BrowserRouter>
  )
+
 }
 
 export default App
---
+++ client/src/pages/Drink.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios'
+
+class MyDrink extends React.Component {
+  state = { drink: {} }
+
+
+componentDidMount(){
+  axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=14029`)
+   .then(response => this.setState({drink: response.data.drinks[0]}))
+}
+
+  render(){
+    console.log('rendering', this.state.drink)
+    return(
+      <h1>hiiiiii</h1> 
+    )
+  }
+}
+
+
+<form>
+  <input
+    type=""
+</form>
+
+
+
+export default MyDrink
+
