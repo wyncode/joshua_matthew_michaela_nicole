c0832652a37021e044f74831c1235bcb94d680e9
Matthew <matthew.o.palm@gmail.com>
2019-07-15 14:25:47 -0400

    styled home page and navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,10 +3,40 @@ import Search from '../components/Search'
 
 const Home = () => {
   return( 
-    <div>
-        <h1 id="makedrink">Make Your Drink</h1>
-        <Search/>
-    </div>
+        //  <React.Fragment>
+
+          <div id="container">
+
+              <div id="Lorem-ipsum-dolor-am">
+                Lorem ipsum dolor amet, consectetur
+              </div>
+
+              <div id="Lorem-ipsum-dolor-si">
+                Lorem ipsum dolor sit amet,consectetur
+                adipisiging elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore
+              </div>
+
+              <div id="button-box">
+              <button id="button">Lorem ipsum</button>
+                {/* Lorem ipsum dolor sit amet,consectetur
+                adipisiging elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore */}
+              </div>
+
+              {/* <div id="button-box">
+              <button type="button" id="button">Lorem ipsum</button>
+              </div> */}
+
+              {/* <div id="button-box">
+
+                <button type="button" id="button">Lorem ipsum</button>
+              </div> */}
+
+          </div>
+          
+         
+         
   )
 }
 
