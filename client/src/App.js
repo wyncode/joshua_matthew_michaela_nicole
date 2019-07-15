4d5cfd6aed8c4c166675ea1242a5607ab099e102
Michaela Carrera <carrera.michaela104@gmail.com>
2019-07-15 14:25:03 -0400

    random_drink

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import RandomDrink from './pages/RandomDrink'
 // import Drink from './pages/Drink'
 
 const App = () => {
  return (
    <BrowserRouter>
       <Navbar/>
+      <Route exact path="/random-drink" component={RandomDrink}/>
      <Route exact path="/" component={Home} />
      {/* <Route exact path="/" component={Drink} /> */}
    </BrowserRouter>
---
+++ client/src/utils/index.js
@@ -0,0 +1,1 @@
+export const startCase = str => str[0].toUpperCase() + str.substr(1).toLowerCase()
