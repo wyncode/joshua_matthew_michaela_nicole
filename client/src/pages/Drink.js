016718ae5fe17ec2df47f12d0c7fb56230874902
Josh <jmart790@fiu.edu>
2019-07-15 19:01:51 -0400

    fixed some styling issues with search

--- client/src/pages/Drink.js
+++ client/src/pages/Drink.js
@@ -4,16 +4,25 @@ import axios from 'axios';
 class MyDrink extends React.Component {
   state = { drink: {} };
 
-  componentDidMount() {
-    axios
-      .get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=14029`)
-      .then(response => this.setState({ drink: response.data.drinks[0] }));
-  }
 
-  render() {
-    console.log('rendering', this.state.drink);
-    return <h1>hiiiiii</h1>;
+componentDidMount(){
+  axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=14029`)
+   .then(response => this.setState({drink: response.data.drinks[0]}))
+}
+
+  render(){
+    console.log('rendering', this.state.drink)
+    return(
+      <div>
+      <h3>My Drink</h3>
+      <h3>Ingredients</h3>
+      <h3>Directions</h3> 
+      </div>
+    )
   }
 }
 
-export default MyDrink;
+
+
+export default MyDrink
+
